c99e8f5984b640a0535ef21b6bc9a33850ba2239
Mattia Marchesini <mattia.marchesini@dinova.one>
2026-02-10 16:31:44 +0100

    add comments before actually implementing custom-controller.go

--- custom-resource/custom-controller.go
+++ custom-resource/custom-controller.go
@@ -1,6 +1,9 @@
 // A custom controller for MyResource.
-
 // Visit https://github.com/kubernetes/sample-controller for an actual example
+
+// - Kubebuilder: A popular framework for building basic Kubernetes operators using Go.
+// - Operator SDK: Another widely used toolkit for developing operators, supporting multiple languages.
+// - Client-Go: For lower-level development, provides libraries to interact with the Kubernetes API.
 
 package myresource
 var controllerKind = apps.SchemeGroupVersion.WithKind("MyResource")
